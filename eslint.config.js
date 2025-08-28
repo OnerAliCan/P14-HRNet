f6a0d4642b89e5970c550a470263b8468f5f6a03
unknown <alican.oner.pro@gmail.com>
2025-08-29 01:04:44 +0200

    config base

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+      'react/react-in-jsx-scope': 'off',
+      'indent': ['error', 2], // ⚡ 2 espaces par niveau
+    }    
   },
 ])
